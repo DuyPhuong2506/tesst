8eaaa07add7630def0923290db18dc6237a1ce48
BAPVN <anhpmt@bap.jp>
2021-10-18 16:58:40 +0700

    create migrate demo

---
+++ database/migrations/2021_10_18_091703_create_customers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('username')->unique(true);
+            $table->string('email');
+            $table->string('password');
+            $table->bigInteger('wedding_id');
+            $table->tinyInteger('type');
+            $table->string('phone');
+            $table->string('address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
---
+++ database/migrations/2021_10_18_091719_create_weddings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWeddingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('weddings', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('place_id');
+            $table->datetime('schedule_starttime');
+            $table->datetime('schedule_endtime');
+            $table->boolean('is_close')->default(false);
+            $table->text('note');
+            $table->string('img_couple');
+            $table->text('thank_msg');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('weddings');
+    }
+}
