d52d3f79e3593ffa0d29e7b8fbae7f76e0086513
Danh Đạt <datd@bap.jp>
2021-11-12 17:31:06 +0700

    bo sung file

---
+++ app/Http/Requests/NewEmailRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\ApiRequest;
+
+class NewEmailRequest extends ApiRequest
+{
+   
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'email' => 'required|email|min:4|max:50'
+        ];
+    }
+}
---
+++ resources/views/mails/admin_staff_invite.blade.php
@@ -0,0 +1,9 @@
+<h1>INVITE STAFF ADMIN REGISTRY ACCOUNT</h1>
+<div>
+    <span>Token: </span>
+    <a href="{{$app_url}}/admin-staff-registry/{{$token}}">{{$token}}</a>
+</div>
+<div>
+    <b>Link: </b>
+    <span>{{$app_url}}/admin-staff-registry/{{$token}}">{{$token}}</span>
+</div>
