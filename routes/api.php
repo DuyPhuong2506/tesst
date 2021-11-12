d98c3f1c6b66be18539f5cb78e423d10ed212465
Danh Đạt <datd@bap.jp>
2021-11-12 11:35:34 +0700

    group api mypage

--- routes/api.php
+++ routes/api.php
@@ -36,9 +36,12 @@ Route::group(['middleware' => ['jwtAuth']], function () {
 
     /**My Page - Detail Account**/
     Route::get('users/get-me','UsersController@getMe');
-    Route::put('users/staff-admin/update', 'UsersController@updateStaffAdminInfo');
-    Route::put('users/super-admin/email/update', 'UsersController@updateSupperAdminEmail');
-    Route::put('users/super-admin/password-verify/update', 'UsersController@updateSupperAdminPassword');
+
+    Route::prefix('users')->group(function () {
+        Route::put('/staff-admin/update', 'UsersController@updateStaffAdminInfo');
+        Route::put('/super-admin/email/update', 'UsersController@updateSupperAdminEmail');
+        Route::put('/super-admin/password-verify/update', 'UsersController@updateSupperAdminPassword');
+    });
 
     Route::post('auth/logout', 'AuthController@logout');
     Route::post('admin/create','UsersController@createAdmin');
