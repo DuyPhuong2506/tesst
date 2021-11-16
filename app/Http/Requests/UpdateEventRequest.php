31ae72323f2779be077f659f092920009f792a23
Danh Đạt <datd@bap.jp>
2021-11-16 13:53:23 +0700

    change date and validate event date

--- app/Http/Requests/UpdateEventRequest.php
+++ app/Http/Requests/UpdateEventRequest.php
@@ -17,8 +17,8 @@ class UpdateEventRequest extends ApiRequest
     {
         return [
             'id'=>'required|exists:weddings,id',
-            'event_name'=>'required|max:100',
-            'date'=>'required|date',
+            'event_name'=>'required|max:100|string',
+            'date'=>'required|date_format:Y-m-d H:i',
             'welcome_start'=>'required|date_format:H:i',
             'welcome_end'=>'required|date_format:H:i',
             'wedding_start'=>'required|date_format:H:i',
@@ -26,10 +26,10 @@ class UpdateEventRequest extends ApiRequest
             'reception_start'=>'required|date_format:H:i',
             'reception_end'=>'required|date_format:H:i',
             'place_id'=>'required|exists:places,id',
-            'groom_name'=>'required|max:30|min:2',
-            'groom_email'=>'email|max:30',
-            'bride_name'=>'required|max:30|min:2',
-            'bride_email'=>'email|max:30',
+            'groom_name'=>'required|max:30|min:2|string',
+            'groom_email'=>'email|max:30|string',
+            'bride_name'=>'required|max:30|min:2|string',
+            'bride_email'=>'email|max:30|string',
         ];
     }
 }
