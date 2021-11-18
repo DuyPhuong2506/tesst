077a87e9884c9f5c983bfca9c93e7aac00c67a69
Danh Đạt <datd@bap.jp>
2021-11-18 15:03:00 +0700

    update email template

--- resources/views/mails/admin_staff_invite.blade.php
+++ resources/views/mails/admin_staff_invite.blade.php
@@ -1,9 +1,27 @@
 <h1>INVITE STAFF ADMIN REGISTRY ACCOUNT</h1>
+<style>
+    .btn{
+        display: inline-block;
+        padding: 5px 20px;
+        background: #3498db;
+        border-radius: 4px;
+        cursor: pointer;
+        color: white;
+        text-decoration: none;
+        margin-top: 15px;
+    }
+    .d-block{
+        display: block;
+    }
+    .center-child{
+        widows: 100%;
+        text-align: center;
+    }
+</style>
 <div>
     <span>Token: </span>
-    <a href="{{$app_url}}/reset-password?token={{$token}}">{{$token}}</a>
-</div>
-<div>
-    <b>Link: </b>
-    <span>{{$app_url}}/reset-password?token={{$token}}</span>
+    <a class="d-block" href="{{$app_url}}/reset-password?token={{$token}}">{{$token}}</a>
+    <div class="center-child">
+        <a class="btn" style="" href="{{$app_url}}/reset-password?token={{$token}}">Click Here</a>
+    </div>
 </div>
