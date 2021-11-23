b23eadcd7ef464aebee1bf1acda4a32259ddf292
Danh Đạt <datd@bap.jp>
2021-11-23 13:43:27 +0700

    fix login email regex

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -16,7 +16,7 @@ class LoginRequest extends ApiRequest
     public function rules()
     {
         return [
-            'email' => 'required|string|email|exists:users,email|max:50|regex:/^[a-zA-Z0-9@._-]+$/',
+            'email' => 'required|string|email|exists:users,email|max:50|regex:/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix',
             'password' => 'required|string|min:8|max:255|regex:/^[a-zA-Z0-9]+$/',
         ];
     }
