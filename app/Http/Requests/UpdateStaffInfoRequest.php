de9c005599cfd42be51d6772039391e9343bb55d
Danh Đạt <datd@bap.jp>
2021-11-24 09:56:23 +0700

    update

--- app/Http/Requests/UpdateStaffInfoRequest.php
+++ app/Http/Requests/UpdateStaffInfoRequest.php
@@ -21,7 +21,7 @@ class UpdateStaffInfoRequest extends ApiRequest
             'contact_email' => 'required|string|email|max:50|regex:/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix',
             'phone' => 'required|numeric|min:11',
             'company_name' => 'required|string|max:50',
-            'post_code' => 'required|max:7',
+            'post_code' => 'required|numeric|max:7',
             'address_1' => 'required|max:200',
             'address_2' => 'required|max:200'
         ];
