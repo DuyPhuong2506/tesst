3ef78c8aa4d8dd3bc908f44cbfa0909496ca38df
Danh Đạt <datd@bap.jp>
2021-11-25 08:11:29 +0700

    update

--- app/Services/EventService.php
+++ app/Services/EventService.php
@@ -166,6 +166,6 @@ class EventService
                             }])
                             ->with(['eventTimes'])
                             ->whereId($eventId)
-                            ->get();
+                            ->first();
     }
 }
