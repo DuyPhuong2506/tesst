3d769767a6d0cc82f09c6e2d38ab720a62f24e04
Danh Đạt <datd@bap.jp>
2021-12-08 10:47:51 +0700

    fix message response when it failed

--- app/Http/Requests/EventLiveStreamRequest.php
+++ app/Http/Requests/EventLiveStreamRequest.php
@@ -3,6 +3,11 @@
 namespace App\Http\Requests;
 
 use App\Http\Requests\ApiRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Response;
 
 class EventLiveStreamRequest extends ApiRequest
 {
@@ -24,5 +29,15 @@ class EventLiveStreamRequest extends ApiRequest
             'token.required' => __('messages.event.validation.token.required'),
             'token.exists' => __('messages.event.validation.token.exists'),
         ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        $response = $this->respondError(
+            Response::HTTP_BAD_REQUEST, 
+            __('messages.event.validation.token.exists')
+        );
+
+        throw new HttpResponseException($response);
     }
 }
