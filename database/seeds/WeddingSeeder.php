cc6fd69752651c0e7c48cd47e7f3986620233d71
Danh Đạt <datd@bap.jp>
2021-12-13 16:38:18 +0700

    seeder

--- database/seeds/WeddingSeeder.php
+++ database/seeds/WeddingSeeder.php
@@ -17,14 +17,15 @@ class WeddingSeeder extends Seeder
      */
     public function run()
     {
+        $faker = Faker\Factory::create();
 
         $restaurant = Restaurant::create([
-            'name' => 'Restaurant 1',
-            'phone' => '1'
+            'name' => $faker->name,
+            'phone' => $faker->phoneNumber
         ]);
 
         $place = Place::create([
-            'name' => 'Place 1',
+            'name' => $faker->name,
             'restaurant_id' => $restaurant->id,
             'status' => "1"
         ]);
@@ -32,14 +33,14 @@ class WeddingSeeder extends Seeder
         $wedding = Wedding::create([
             'place_id' => $place->id,
             'date' => '2021-12-13 15:00:00',
-            'title' => "Wedding 2022 testing...",
-            'pic_name' => "Pic Name test...",
+            'title' => $faker->title,
+            'pic_name' => $faker->name,
             "ceremony_reception_time" => "06:00-07:00",
             "ceremony_time" => "07:00-08:00",
             "party_reception_time" => "08:00-09:00",
             "party_time" => "09:00-10:00",
-            "greeting_message" => "Greeing Message for wedding testing...",
-            "thank_you_message" => "Thank you Message for wedding testing...",
+            "greeting_message" => $faker->paragraph,
+            "thank_you_message" => $faker->paragraph,
             "guest_invitation_response_date" => "2021-12-13",
             "couple_edit_date" => "2021-12-13",
         ]);
@@ -61,67 +62,41 @@ class WeddingSeeder extends Seeder
         ]);
 
         Customer::create([
-            'username' => Str::random(12),
-            'full_name' => 'Groom_Name',
-            'email' => 'groom@mail.com',
-            'token' => '12345678',
+            'username' => $faker->unique()->userName,
+            'full_name' => $faker->name,
+            'email' => $faker->email,
+            'token' => "87654321",
             'password' => '111111111111',
             'wedding_id' => $wedding->id,
             'role' => "3"
         ]);
 
         Customer::create([
-            'username' => Str::random(12),
-            'full_name' => 'Bride_Name',
-            'email' => 'bride@mail.com',
-            'token' => '876543121',
+            'username' => $faker->unique()->userName,
+            'full_name' => $faker->name,
+            'email' => $faker->email,
+            'token' => "12345678",
             'password' => '222222222222',
             'wedding_id' => $wedding->id,
             'role' => "4"
         ]);
 
-        Customer::create([
-            'username' => Str::random(12),
-            'full_name' => 'GUEST_1',
-            'email' => '333333333333@mail.com',
-            'token' => '333333333333',
-            'password' => '333333333333',
-            'wedding_id' => $wedding->id,
-            'role' => "5",
-            "table_position_id" => $tableA->id
-        ]);
-
-        Customer::create([
-            'username' => Str::random(12),
-            'full_name' => 'GUEST 2',
-            'email' => '444444444444@mail.com',
-            'token' => '444444444444',
-            'password' => '444444444444',
-            'wedding_id' => $wedding->id,
-            'role' => "5",
-            "table_position_id" => $tableA->id
-        ]);
-        
-        Customer::create([
-            'username' => Str::random(12),
-            'full_name' => 'GUEST 3',
-            'email' => '555555555555@mail.com',
-            'token' => '555555555555',
-            'password' => '555555555555',
-            'wedding_id' => '1',
-            'role' => "5",
-            "table_position_id" => $tableB->id
-        ]);
-
-        Customer::create([
-            'username' => Str::random(12),
-            'full_name' => 'GUEST 4',
-            'email' => '666666666666@mail.com',
-            'token' => '666666666666',
-            'password' => '666666666666',
-            'wedding_id' => '1',
-            'role' => "5",
-            "table_position_id" => $tableB->id
-        ]);
+        $listCustomer = [];
+        for($i = 0; $i < 10; $i++){
+            $arrayId = [$tableA->id, $tableB->id];
+            $tableId = $arrayId[array_rand($arrayId)];
+            $item = [
+                'username' => $faker->unique()->userName,
+                'full_name' => $faker->name,
+                'email' => $faker->email,
+                'token' => Str::random(12),
+                'password' => '222222222222',
+                'wedding_id' => $wedding->id,
+                'role' => "5",
+                "table_position_id" => $tableId
+            ];
+            array_push($listCustomer, $item);
+        }
+        DB::table('customers')->insert($listCustomer);
     }
 }
